0ed09d389efab3d856e1e7efa53cbff60f5dc4bb
Brandan Jones <jonesbr@ucmail.uc.edu>
2023-11-20 17:09:31 -0500

    Upload and validate XML.

--- PlantPlaces23FS7024001/Pages/UploadPlantXml.cshtml.cs
+++ PlantPlaces23FS7024001/Pages/UploadPlantXml.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Xml;
+using System.Xml.Schema;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace PlantPlaces23FS7024001.Pages
@@ -33,7 +34,45 @@ namespace PlantPlaces23FS7024001.Pages
             XmlNode? xmlNode = doc.SelectSingleNode("/plant/genus");
             int foo = 1 + 1;
 
+            // Let's Validate this file.
+            ValidateXml(file);
+
         }
 
+        private void ValidateXml(string file)
+        {
+            // let's define the schema we're going to use to validate our XML.
+            XmlReaderSettings settings = new XmlReaderSettings();
+            string xsdPath = Path.Combine(environment.ContentRootPath, "uploads", "plants.xsd");
+            settings.Schemas.Add(null, xsdPath);
+
+            // we want to validate with XSD
+            settings.ValidationType = ValidationType.Schema;
+
+            // set validation flags
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            // if something goes wrong, use ValidationEventHandler method to report it.
+            settings.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(this.ValidationEventHandler);
+
+            XmlReader xmlReader = XmlReader.Create(file, settings);
+
+            try { 
+                while (xmlReader.Read())
+                {
+
+                }
+                ViewData["UploadParseResult"] = "Validation Passed.";
+            } catch (Exception ex)
+            {
+                ViewData["UploadParseResult"] = ex.Message;
+            }
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs args)
+        {
+            throw new Exception ("Validation Failed.  Message: " + args);
+        }
     }
 }
